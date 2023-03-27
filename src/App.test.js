1cf71eab67aa23bc860d3992f0c934678ea5deaa
Iker Sánchez <iker.sanchez@kairosds.com>
2023-03-27 17:06:05 +0200

    feat: test cache

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,25 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import CacheService from "./services/CacheService";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("CacheService", () => {
+  it("should return the same instance when calling getInstance()", () => {
+    const instance1 = CacheService.getInstance();
+    const instance2 = CacheService.getInstance();
+
+    expect(instance1).toBe(instance2);
+  });
+
+  it("should cache items correctly", () => {
+    const cacheService = CacheService.getInstance();
+
+    // Insert items into the cache
+    cacheService.cache.set("key1", "value1");
+    cacheService.cache.set("key2", "value2");
+
+    // Retrieve items from the cache
+    const value1 = cacheService.cache.get("key1");
+    const value2 = cacheService.cache.get("key2");
+
+    expect(value1).toEqual("value1");
+    expect(value2).toEqual("value2");
+  });
 });
